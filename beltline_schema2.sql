f100c3f10627827e26dd61c288154104178b21fd
patrickc410 <39349028+patrickc410@users.noreply.github.com>
2019-03-22 15:54:20 -0400

    Update beltline_schema2.sql

--- beltline_schema2.sql
+++ beltline_schema2.sql
@@ -2,25 +2,31 @@ DROP DATABASE IF EXISTS beltline;
 CREATE DATABASE beltline;
 USE beltline;
 
--- SET NAMES utf8; -- what is this?
 
 
 
 DROP TABLE IF EXISTS user;
--- SET character_set_client = utf8mb4 ; -- what is this?
 CREATE TABLE user (
     username varchar(20) PRIMARY KEY,
     user_type ENUM('Visitor','Employee','Both'),
     fname varchar(20),
     lname varchar(20),
-    status ENUM('Administrator','Staff','Manager'),
+    status ENUM('Approved', 'Declined', 'Pending'),
     password varchar(20)
 );
 
 
+INSERT INTO user VALUES
+    ('user1', 'Visitor', 'Patrick', 'Crawford', 'Approved', 'password1'),
+    ('user2', 'Employee', 'Madison', 'Smith', 'Declined', 'password2'),
+    ('user3', 'Both', 'Lauren', 'Johnson', 'Pending', 'password3'),
+    ('user4', 'Visitor', 'Katie', 'Neil', 'Approved', 'password4'),
+    ('user5', 'Both', 'Abbey', 'Nannis', 'Declined', 'password5');
+
+
+
 
 DROP TABLE IF EXISTS email;
--- SET character_set_client = utf8mb4 ; -- what is this?
 CREATE TABLE email (
     username varchar(20),
     email varchar(100),
@@ -29,8 +35,25 @@ CREATE TABLE email (
 );
 
 
+INSERT INTO email VALUES
+    ('user1', 'user1_email1_@gmail.com'),
+    ('user2', 'user2_email1_@aol.com'),
+    ('user2', 'user2_email2_@aol.com'),
+    ('user3', 'user3_email1_@bellsouth.net'),
+    ('user3', 'user3_email2_@bellsouth.net'),
+    ('user3', 'user3_email3_@bellsouth.net'),
+    ('user4', 'user4_email1_@yahoo.com'),
+    ('user4', 'user4_email2_@yahoo.com'),
+    ('user4', 'user4_email3_@yahoo.com'),
+    ('user4', 'user4_email4_@yahoo.com'),
+    ('user5', 'user5_email1_@icloud.com'),
+    ('user5', 'user5_email2_@icloud.com'),
+    ('user5', 'user5_email3_@icloud.com'),
+    ('user5', 'user5_email4_@icloud.com'),
+    ('user5', 'user5_email5_@icloud.com');
+
+
 DROP TABLE IF EXISTS employee;
--- SET character_set_client = utf8mb4 ; -- what is this?
 CREATE TABLE employee (
     employeeID decimal(10,0) PRIMARY KEY,
     username varchar(20) UNIQUE KEY,
@@ -39,13 +62,14 @@ CREATE TABLE employee (
     city varchar(20),
     state varchar(15),
     zipcode int(9),
+    employee_type ENUM('Administrator','Staff','Manager'),
+    CHECK (username in (select username from user where user_type = 'Employee' or user_type = 'Both')),
     CONSTRAINT employee_fk1 FOREIGN KEY (username) REFERENCES user(username)
 );
 
 
 
 DROP TABLE IF EXISTS site;
--- SET character_set_client = utf8mb4 ; -- what is this?
 CREATE TABLE site (
     name varchar(40) PRIMARY KEY,
     address varchar(40),
@@ -60,7 +84,6 @@ CREATE TABLE site (
 
 
 DROP TABLE IF EXISTS transit;
--- SET character_set_client = utf8mb4 ; -- what is this?
 CREATE TABLE transit (
     type varchar(20),
     route varchar(20),
@@ -71,16 +94,15 @@ CREATE TABLE transit (
 
 
 DROP TABLE IF EXISTS event;
--- SET character_set_client = utf8mb4 ; -- what is this?
 CREATE TABLE event (
     name varchar(40),
-    start_date char(10),
+    start_date date,
     site_name varchar(40),
     description varchar(100),
     min_staff_req decimal(4,0),
     capacity decimal(5,0),
     price decimal(3,2),
-    end_date char(10),
+    end_date date,
     PRIMARY KEY (name, start_date, site_name),
     CONSTRAINT event_fk1 FOREIGN KEY (site_name) REFERENCES site (name)
 );
@@ -88,12 +110,11 @@ CREATE TABLE event (
 
 
 DROP TABLE IF EXISTS take_transit;
--- SET character_set_client = utf8mb4 ; -- what is this?
 CREATE TABLE take_transit (
     username varchar(20),
     transit_type varchar(20),
     route varchar(20),
-    take_date char(10),
+    take_date date,
     PRIMARY KEY (username, transit_type, route, take_date),
     CONSTRAINT take_transit_fk1 FOREIGN KEY (username) REFERENCES user (username),
     CONSTRAINT take_transit_fk2 FOREIGN KEY (transit_type, route) REFERENCES transit (type, route)
@@ -101,11 +122,10 @@ CREATE TABLE take_transit (
 
 
 DROP TABLE IF EXISTS visit_site;
--- SET character_set_client = utf8mb4 ; -- what is this?
 CREATE TABLE visit_site (
     username varchar(20),
     site_name varchar(40),
-    visit_date char(10),
+    visit_date date,
     PRIMARY KEY (username, site_name, visit_date),
     CONSTRAINT visit_site_fk1 FOREIGN KEY (username) REFERENCES user (username),
     CONSTRAINT visit_site_fk2 FOREIGN KEY (site_name) REFERENCES site (name)
@@ -116,11 +136,10 @@ CREATE TABLE visit_site (
 
 
 DROP TABLE IF EXISTS visit_event;
--- SET character_set_client = utf8mb4 ; -- what is this?
 CREATE TABLE visit_event (
     username varchar(20),
     event_name varchar(40),
-    start_date char(10),
+    start_date date,
     site_name varchar(40),
     visit_date char(10),
     PRIMARY KEY (username, event_name, start_date, site_name, visit_date),
@@ -133,7 +152,6 @@ CREATE TABLE visit_event (
 
 
 DROP TABLE IF EXISTS transit_connections;
--- SET character_set_client = utf8mb4 ; -- what is this?
 CREATE TABLE transit_connections (
     site_name varchar(40),
     transit_type varchar(20),
@@ -148,11 +166,10 @@ CREATE TABLE transit_connections (
 
 
 DROP TABLE IF EXISTS event_staff_assignments;
--- SET character_set_client = utf8mb4 ; -- what is this?
 CREATE TABLE event_staff_assignments (
     employeeID decimal(10,0),
     event_name varchar(40),
-    start_date char(10),
+    start_date date,
     site_name varchar(40),
     PRIMARY KEY (employeeID, event_name, start_date, site_name),
     CONSTRAINT event_staff_assignments_fk1 FOREIGN KEY (employeeID) REFERENCES employee (employeeID),
