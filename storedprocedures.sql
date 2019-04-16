cda7546b11a31351fffa0cfb11e9365964b2755c
eljay404 <48839921+eljay404@users.noreply.github.com>
2019-04-16 10:40:51 -0400

    Update storedprocedures.sql

--- storedprocedures.sql
+++ storedprocedures.sql
@@ -58,7 +58,7 @@ CREATE PROCEDURE create_visitor (
  insert into user
     values (inusername, inpassword, "Pending", infname, inlname, "User");
  insert into email
-    values (inusername, inemail)
+    values (inusername, inemail);
  insert into visitor_list
     values (inusername);
  END //
@@ -66,7 +66,9 @@ DELIMITER ;
 
 DROP PROCEDURE if exists check_userphone_unique;
 DELIMITER //
-CREATE PROCEDURE check_userphone_unique (IN inusername VARCHAR(20), IN inphone char(10))
+CREATE PROCEDURE check_userphone_unique (
+    IN inusername VARCHAR(20),
+    IN inphone char(10))
  BEGIN
  select username
  from user
@@ -76,3 +78,32 @@ CREATE PROCEDURE check_userphone_unique (IN inusername VARCHAR(20), IN inphone c
  where (phone = inphone);
  END //
 DELIMITER ;
+
+DROP PROCEDURE if exists create_employee;
+DELIMITER //
+CREATE PROCEDURE create_employee (
+    IN inusername VARCHAR(20),
+    IN inpassword VARCHAR(20),
+    IN infname varchar(20),
+    IN inlname varchar(20),
+    IN inemail varchar(50),
+    IN inphone char(10),
+    IN inaddress varchar(40),
+    IN incity varchar(20),
+    IN instate ENUM('AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA',
+               'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD',
+               'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ',
+               'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC',
+               'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY', 'Other'),
+    IN zipcode char(5),
+    IN inemployee_type ENUM('Admin','Staff','Manager'))
+ BEGIN
+ insert into user
+    values (inusername, inpassword, "Pending", infname, inlname, "User");
+ insert into email
+    values (inusername, inemail);
+ insert into employee(username, phone, address, city, state, zipcode, employee_type)
+    values (inusername, inphone, inaddress, incity, instate, inzipcode, inemployee_type);
+ END //
+DELIMITER ;
+
